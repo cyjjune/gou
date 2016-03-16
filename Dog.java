508aa93c3c20a5c06e1bd73d9d7880a3adf8d744
cyjjune <yunnneltmt@gmail.com>
2016-03-16 11:29:20 -0400

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+  
+  String breed;
+  int age;
+  String name;
+  
+  public Dog( String breed, int age, String name ){
+    this.breed = breed;
+    this.age = age;
+    this.name = name;
+  }
+}
